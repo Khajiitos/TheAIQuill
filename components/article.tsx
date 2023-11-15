5696d85922fef10082023663abc24075af4d97d4
khajiitos <khajiit112@gmail.com>
2023-11-15 12:03:04 +0100

    Fix lint errors

--- components/article.tsx
+++ components/article.tsx
@@ -11,7 +11,7 @@ export default async function Article(props : ArticleProp) {
     const innerHTML = props.articleData != null ? parse(props.articleData.article_content) : "";
 	return (
             <article className="bg-green-800 container-none max-w-7xl min-h-screen m-0 p-10 text-white rounded-sm">
-                {<p className="text-2xl font-semibold mb-7">{props.articleData?.article_header || <span className="text-red-500">Article doesn't exist</span>}</p>}
+                {<p className="text-2xl font-semibold mb-7">{props.articleData?.article_header || <span className="text-red-500">Article doesn&apos;t exist</span>}</p>}
                 <section dangerouslySetInnerHTML={{__html: innerHTML}}></section>
             </article>
 	);
--- components/article_entry.tsx
+++ components/article_entry.tsx
@@ -1,5 +1,6 @@
 import Link from "next/link";
 import { ArticleInfo } from "@/types/articles";
+import Image from "next/image";
 
 export default async function ArticleEntry(props: {articleInfo: ArticleInfo}) {
     const wordCount: number = props.articleInfo.article_content.split(" ").length;
@@ -19,7 +20,7 @@ export default async function ArticleEntry(props: {articleInfo: ArticleInfo}) {
             <div className="bg-green-600 hover:bg-green-500 text-white container-lg rounded m-4 p-3 transition-colors">
                 <p className="text-lg font-semibold">{props.articleInfo.article_header}</p>
                 <p className="text-gray-300 text-sm">{props.articleInfo.article_description}</p>
-                <p className="mt-2 text-gray-300 text-sm"><img src="/img/clock_gray.svg" width="16px" className="inline mr-2"></img>{minutesOfReading} min read | {formattedDate}</p>
+                <p className="mt-2 text-gray-300 text-sm"><Image src="/img/clock_gray.svg" width={16} className="inline mr-2" alt="Reading time"></Image>{minutesOfReading} min read | {formattedDate}</p>
             </div>
         </Link>
 	);
