7b078f1e902939e92aacce983ba44587ef8ab3a0
khajiitos <khajiit112@gmail.com>
2023-11-22 22:03:02 +0100

    fix lint error

--- components/google_analytics.tsx
+++ components/google_analytics.tsx
@@ -7,7 +7,7 @@ export default async function GoogleAnalytics(props: {trackingID: string}) {
             src={`https://www.googletagmanager.com/gtag/js?id=${props.trackingID}`}
             strategy="afterInteractive"
           />
-          <Script strategy="afterInteractive">
+          <Script id="google-analytics" strategy="afterInteractive">
             {`
             window.dataLayer = window.dataLayer || [];
               function gtag(){dataLayer.push(arguments);}
